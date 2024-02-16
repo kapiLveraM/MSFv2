ed99eb0f9125ed336949173604fe9eea76eefcca
kapil verma <vermakp39@gmail.com>
2024-02-16 15:27:51 +0530

    first commit

--- src/components/OurBlogs.jsx
+++ src/components/OurBlogs.jsx
@@ -7,28 +7,6 @@ import Link from "next/link";
 import Slider from "react-slick";
 
 const OurBlogs = () => {
-  const sliderRef = useRef(null);
-  var cardSlider = {
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 725,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
-  const prevSlideHandler = () => {
-    sliderRef.current.slickPrev();
-  };
 
   return (
     <div className=" mb-20 lg:mb-[106px] relative ">
